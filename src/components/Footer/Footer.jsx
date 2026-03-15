45b77da91d8b90b754613be83b3a931dbc72efca
Emmanuel michael <officialimmanuelmichael@gmail.com>
2026-03-15 03:43:20 -0700

    add footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import './Footer.css'; 
+// import './Footer.css'; 
 
 
 const Footer = () => {
